d501f6136fc8e8316ffc7f328c0bde6dc5513758
kelvinmwai <muthikelvin22@gmail.com>
2023-09-18 11:56:55 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
  * @format: pointer to array of characters
@@ -9,56 +7,53 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int x, char_prnt = 0, printed;
-	int flags, width, precision, size, buff_ind = 0;
+	int char_prnt = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
 
-	if (format ==  NULL)
+	if (format == NULL)
 		return (-1);
-
 	va_start(list, format);
-
-	for (x = 0; format && format[x] != '\0'; x++)
+	while (*format)
 	{
-		if (format[x] != '%')
+		if (*format != '%')
 		{
-			buffer[buff_ind++] = format[x];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+			write(1, format, 1);
 			char_prnt++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &x);
-			width = get_width(format, &x, list);
-			precision = get_precision(format, &x, list);
-			size = get_size(format, &x);
-			++x;
-			printed = form_ident(format, &x, list, buffer,
-				flags, width, precision, size);/*identifies format c,d,i,%*/
-			if (printed == -1)
-				return (-1);
-			char_prnt += printed;
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				char_prnt++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(list, int);
+
+				write(1, &c, 1);
+				char_prnt++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(list, char*);
+				int str_len = 0;
+				int x;
+
+				for (x = 0; str[x] != '\0'; x++)
+				{
+					str_len++;
+				}
+				write(1, str, str_len);
+				char_prnt += str_len;
+			}
 		}
+		format++;
 	}
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-
-	return (char_prnt);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer
- * @buffer: stores array of chars
- * @buff_ind: Index at which to add next char || the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+va_end(list);
+return (char_prnt);
 }
 
